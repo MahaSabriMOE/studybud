f7d493723c50a72c22c28ec838a9e602fda9bc0f
MahaSabriMOE <maha.sabri@moe.edu.ps>
2023-04-19 10:29:24 +0200

    this is my project

---
+++ base/views.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, redirect
+from .models import Room, Topic
+from .forms import RoomForm
+
+
+
+# Create your views here.
+
+
+def home (request):
+    rooms=Room.objects.all()
+    topics=Topic.objects.all()
+    context={'rooms': rooms,'topics': topics}
+    return render(request, 'base/home.html',context)
+
+def room(request,pk):
+    room=Room.objects.get(id=pk)
+    context={'room':room}
+    return render(request,'base/room.html',context)
+
+def createRoom(request):
+    form=RoomForm()
+    if request.method == 'POST':
+        form=RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+       
+    
+    
+    context={'form': form}
+    return render (request, 'base/room_form.html',context)
+
+def updateRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    form=RoomForm(instance=room)
+    if request.method == 'POST':
+        form=RoomForm(request.POST,instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    
+    contxet={'form': form}
+    return render(request,'base/room_form.html',contxet)
+
+
+    
+def deleteRoom(request,pk):
+    room=Room.objects.get(id=pk)
+    if request.method=='POST':
+        room.delete()
+        return redirect('home')
+    return render(request , 'base/delete.html' ,{'obj':room})    
