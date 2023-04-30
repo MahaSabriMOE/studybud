3480b0dfc08be1431884e7da33e73c42f978df64
MahaSabriMOE <maha.sabri@moe.edu.ps>
2023-04-30 09:05:05 +0300

    mychnges1

--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -8,9 +9,19 @@ from .forms import RoomForm
 
 
 def home (request):
-    rooms=Room.objects.all()
+    q=request.GET.get('q') if request.GET.get('q') !=None else ''
+    rooms=Room.objects.filter(
+       Q(topic__name__icontains=q) |
+       Q(name__icontains=q) |
+       Q(description__icontains=q)
+           
+       
+       
+        )
+    
     topics=Topic.objects.all()
-    context={'rooms': rooms,'topics': topics}
+    room_count=rooms.count()
+    context={'rooms': rooms,'topics': topics , 'room_count': room_count}
     return render(request, 'base/home.html',context)
 
 def room(request,pk):
